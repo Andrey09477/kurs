2ba8b37e3a1cdc01a70a618f3f04424fbbe4e337
Andrey <90399811+Andrey09477@users.noreply.github.com>
2025-05-23 17:19:52 +0300

    Create const.py

---
+++ const.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class Column(Enum):
+    ID = 'id'
+    LAST_NAME = 'last_name'
+    FIRST_NAME = 'first_name'
+    GENDER = 'gender'
+    WEIGHT = 'weight'
+    HEIGHT = 'height'
+
+MAX_WEIGHT = 150
+MAX_HEIGHT = 200
